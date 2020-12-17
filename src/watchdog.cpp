afd637674abd8ea665d040c16b8bf41a5d04574b
tahakalkanli <abdullahtahakalkanli@gmail.com>
2020-12-18 00:38:43 +0300

    slow process

--- src/watchdog.cpp
+++ src/watchdog.cpp
@@ -10,25 +10,43 @@
 */
 #include <string> 
 #include <unistd.h>
+#include <fstream>
+#include <fcntl.h> 
+#include <sys/stat.h> 
 
 using namespace std;
 int num_of_processes;
 char* process_output;
-string watchdog_output;
+char* watchdog_output;
 
 int main(int argc, char const *argv[])
 {   
     num_of_processes = stoi(argv[1]);
     process_output = (char*) argv[2];
-    watchdog_output = argv[3];
+    watchdog_output = (char*) argv[3];
+
+    ofstream myfile;
+    myfile.open(watchdog_output);
+    int unnamedPipe;
+    char * myfifo = (char*) "/tmp/myfifo";
+    mkfifo(myfifo, 0644);
+    char temp[30];
+    string message;
+    unnamedPipe = open(myfifo,O_WRONLY);
 
     for(int i=0; i<num_of_processes; i++){
         pid_t child = fork();
         if(child == 0) {
             char process_number = char(i);
-            execl(process_output, (char*)process_number);
+            execl("./executor.out", (char*)process_number ,process_output); // might not be correct to way.
+            temp[30] = 'P' + i + ' ' + getpid();
+            write(unnamedPipe, temp, 30);
         }
     }
-    
+
+    /*
+        wait for signals. then do propoper operation.
+    */
+
     return 0;
 }
