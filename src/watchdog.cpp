7b6dcd7bb97a0aae5d6fb1fe094ce3af85d44a1f
tahakalkanli <abdullahtahakalkanli@gmail.com>
2020-12-18 02:50:40 +0300

    0250 :)

--- src/watchdog.cpp
+++ src/watchdog.cpp
@@ -4,6 +4,8 @@
 #include <fstream>
 #include <fcntl.h> 
 #include <sys/stat.h> 
+#include<sys/wait.h> 
+
 
 
 using namespace std;
@@ -30,13 +32,14 @@ int main(int argc, char const *argv[])
 
     temp[30] = 'P0 ' + getpid(); 
     write(unnamedPipe, temp, 30);
-
-    for(int i=1; i<=num_of_processes; i++) {
+    int i, pid;
+    char process_number;
+    for(i=1; i<=num_of_processes; i++) {
 
         pid_t child = fork();
         if(child == 0) {
-            char process_number = char(i);
-            int pid = getpid();
+            process_number = char(i);
+            pid = getpid();
             temp[30] = 'P' + i + ' ' + pid;
             write(unnamedPipe, temp, 30);
             output[50] = 'P' + i + ' is started and it has a pid of ' + pid;
@@ -52,6 +55,16 @@ int main(int argc, char const *argv[])
     while(1) {
         sleep(1);
     }
+
+    while(wait(NULL) > 0) {
+        if(i == 1) {
+            cout << "kill' em all " << i << endl;
+            // kill all processes. reinitialize all processes.
+        } else {
+            cout << "time to reinitialize " << i << endl;
+        }
+    }
+    
     /*
         wait for signals. then do propoper operation.
         wait is necessary since we want watchdog to wait for child processes.
